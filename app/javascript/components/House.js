845ce200ba6a1bf81e8686ee12d2857158058162
Ali <alihbouran@gmail.com>
2022-07-12 11:53:34 -0700

    set state for users and houses

--- app/javascript/components/House.js
+++ app/javascript/components/House.js
@@ -1,6 +1,32 @@
 import React from 'react'
+import axios from 'axios'
+import { useState, useEffect } from 'react';
 
 function House() {
+  const [state, setState] = useState({
+    attributes: {},
+    roommates: []
+  })
+
+
+  useEffect(() => { 
+  axios.get('http://localhost:3000/api/v1/houses')
+  .then((result) => {
+    setState(prev => ({
+    ...prev,
+    attributes: result.data.data[0].attributes
+}))
+  })
+  axios.get('http://localhost:3000/api/v1/users')
+  .then((result) => {
+    setState(prev => ({
+      ...prev,
+      roommates: [result.data.data]
+    }))
+})}, [])
+
+  console.log('state', state.attributes)
+  console.log('state roomates', state.roommates)
   return (
     <div>This is the house/1 page</div>
   )
