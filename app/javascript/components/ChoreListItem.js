95b9abbdcc8cff9f83b05ee0fe29be1ba0cb53ec
Ali <alihbouran@gmail.com>
2022-07-16 12:52:10 -0700

    try deleting from database

--- app/javascript/components/ChoreListItem.js
+++ app/javascript/components/ChoreListItem.js
@@ -20,22 +20,13 @@ function ChoreListItem(props) {
      const data = props.chore.id
      const object = props.chore
 
-    // const deleteId = props.chore.id;
-
     axios.delete(`http://localhost:3000/api/v1/chore/${data}`, {data: object})
-    .then((result) => console.log('result', result))
-    
-
-    const name = e.target.getAttribute("name")
-    updateList(list.filter(item => item.name !== name));
-
-    const newChores = [...chores]  
-    const index = newChores.indexOf(data)
-    console.log('chores', chores)
-    console.log('index', index)
-    newChores.splice(index, 1)
-    setChores(newChores);
-  
+    .then((result) => {
+      axios.get('http://localhost:3000/api/v1/houses/1')
+      .then((res) => {
+        setChores(res.data.chores)
+      })
+    })
   }
 
 
