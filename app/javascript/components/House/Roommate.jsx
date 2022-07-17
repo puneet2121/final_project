c56d6965cba423acb8bc96423deb6ff94377f3f5
puneet2121 <bedipuneet21@gmail.com>
2022-07-17 21:05:32 +0000

    add hover to nav and house page

--- app/javascript/components/House/Roommate.jsx
+++ app/javascript/components/House/Roommate.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Card from 'react-bootstrap/Card';
+import '../style/House.scss'
 export default function Roommate(props) {
   console.log('props', props)
   return (
@@ -7,7 +8,7 @@ export default function Roommate(props) {
 
       {props.roommate.map((roommate) => {
         return (
-          <Card border="primary" style={{ width: '28rem' ,marginBottom:'10px',borderRadius: '21px',border: '2px solid black',marginRight:'15px'}}>
+          <Card border="primary" className='hover-overlay' style={{ width: '28rem' ,marginBottom:'10px',borderRadius: '21px',border: '2px solid black',marginRight:'15px'}}>
             <Card.Header><img style={{height:'auto',width:'10%',marginRight:'5%' }} src={ roommate?.image } />{roommate?.name}</Card.Header>
             <Card.Body>
               <Card.Title>{roommate?.bio}
--- app/javascript/components/House/User.jsx
+++ app/javascript/components/House/User.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import Card from 'react-bootstrap/Card';
 import Image from 'react-bootstrap/Image'
 import { useCookies } from "react-cookie";
+import '../style/House.scss'
 export default function User(props) {
   console.log('props', props)
   const [cookies] = useCookies()
   return (
-    <Card border="primary" style={{ width: '30rem', height: '44rem', border: '2px solid black', borderRadius: '21px' , marginLeft:'-18px'}} >
+    <Card border="primary" className='hover-overlay' style={{width: '30rem', height: '44rem', border: '2px solid black', borderRadius: '21px' , marginLeft:'-18px'}} >
       <Card.Img variant="top" src={props?.roommate?.image} style={{ height: '15rem', width: '23rem', marginLeft: '12%', marginTop:'-21px'}} />
       <hr />
       <Card.Title style={{alignText:'center'}}>My Profile <br />{props?.roommate?.name}</Card.Title>
