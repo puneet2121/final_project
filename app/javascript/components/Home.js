da6ef3422a23be25b9c9c7b85654fbd3e6237b40
Ali <alihbouran@gmail.com>
2022-07-17 18:38:14 -0700

    stylizied a little

--- app/javascript/components/Home.js
+++ app/javascript/components/Home.js
@@ -5,6 +5,8 @@ import Form from 'react-bootstrap/Form';
 import { Redirect, useParams } from 'react-router-dom'
 import { useCookies } from "react-cookie";
 import { useState, useEffect } from 'react';
+import '../../assets/stylesheets/chores.css'
+import img from '../src/images/Untitled-3 2.png'
 function Home(props) {
   // const [cookies, setCookie] = useCookies(["user"]);
   const something = useParams()
@@ -44,8 +46,13 @@ function Home(props) {
 
 
   return (
-    
+ 
     <Form>
+      <h1 className='something'>Welcome to Ripple</h1>
+        <div className='main-thing'>
+          <img src={img} className='house-image' />
+          <h1 className="tag-line">A roommate management system.</h1>
+        </div>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Login</Form.Label>
         <Form.Control type="name" value={props.username} onChange={(event) => props.setUsername(event.target.value)} />
